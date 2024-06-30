fa2a7e4b6550403f17510b1d782d51c61e6fe99e
Владислав Костров <vladislav.kostrov@rydlab.ru>
2024-06-30 22:29:07 +0500

    Finish task

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,85 @@
-import Cart from '../service/Cart';
+import Cart from "../service/Cart";
+import Movie from "../domain/Movie";
+import Book from "../domain/Book";
+import MusicAlbum from "../domain/MusicAlbum";
 
-test('new card should be empty', () => {
+test("new card should be empty", () => {
   const cart = new Cart();
 
   expect(cart.items.length).toBe(0);
+});
+
+test("add value to the chart", () => {
+  const cart = new Cart();
+  const movie = new Movie(
+    1009,
+    "The Avengers",
+    800,
+    2012,
+    "USA",
+    "Avengers Assemle!",
+    ["фантастика", "боевик", "фэнтези", "приключения"],
+    137
+  );
+  cart.add(movie);
+  expect(cart.items[0]).toEqual(movie);
+});
+
+test("return sum of the chart", () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, "War and Piece", "Leo Tolstoy", 2000, 1225));
+  cart.add(new MusicAlbum(1008, "Meteora", "Linkin Park", 900));
+  cart.add(
+    new Movie(
+      1009,
+      "The Avengers",
+      800,
+      2012,
+      "USA",
+      "Avengers Assemle!",
+      ["фантастика", "боевик", "фэнтези", "приключения"],
+      137
+    )
+  );
+  expect(cart.totalAmountClear()).toBe(3700);
+});
+
+test("return sum of the chart with discount", () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, "War and Piece", "Leo Tolstoy", 2000, 1225));
+  cart.add(new MusicAlbum(1008, "Meteora", "Linkin Park", 900));
+  cart.add(
+    new Movie(
+      1009,
+      "The Avengers",
+      800,
+      2012,
+      "USA",
+      "Avengers Assemle!",
+      ["фантастика", "боевик", "фэнтези", "приключения"],
+      137
+    )
+  );
+  expect(cart.totalAmountDiscount(10)).toBe(3330);
+});
+
+test("delete item from the chart", () => {
+  const cart = new Cart();
+  cart.add(new Book(1001, "War and Piece", "Leo Tolstoy", 2000, 1225));
+  cart.add(new MusicAlbum(1008, "Meteora", "Linkin Park", 900));
+  cart.add(
+    new Movie(
+      1009,
+      "The Avengers",
+      800,
+      2012,
+      "USA",
+      "Avengers Assemle!",
+      ["фантастика", "боевик", "фэнтези", "приключения"],
+      137
+    )
+  );
+  cart.deleteItem(1001);
+  const curIds = cart.items.map((item) => item.id);
+  expect(curIds).toEqual([1008, 1009]);
 });
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,13 +1,40 @@
-import Buyable from '../domain/Buyable';
+import Buyable from "../domain/Buyable";
 
 export default class Cart {
-    private _items: Buyable[] = [];
+  private _items: Buyable[] = [];
 
-    add(item: Buyable): void {
-        this._items.push(item);
-    }
+  add(item: Buyable): void {
+    this._items.push(item);
+  }
 
-    get items(): Buyable[] {
-        return [...this._items]; 
+  get items(): Buyable[] {
+    return [...this._items];
+  }
+
+  set items(items: Buyable[]) {
+    this._items = items;
+  }
+
+  totalAmountClear(): number {
+    let sum: number = 0;
+    for (const item of this.items) {
+      sum += item.price;
     }
+    return sum;
+  }
+
+  totalAmountDiscount(discount: number): number {
+    const clearPriceTotal = this.totalAmountClear();
+    return clearPriceTotal - (clearPriceTotal * discount) / 100;
+  }
+
+  deleteItem(id: number): void {
+    const newItemsList = [];
+    for (const item of this.items) {
+      if (item.id !== id) {
+        newItemsList.push(item);
+      }
+    }
+    this.items = newItemsList;
+  }
 }
